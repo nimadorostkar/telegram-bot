2e3f238280121bb4c4eb0de09fb10d584f2e47ab
nima <54316106+nimadorostkar@users.noreply.github.com>
2020-07-22 12:51:29 +0430

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+import json
+
+updater = Updater('1094887685:AAFLLjhqPtR_IKER8crAlgx3qmYHl5rocVE')
+
+
+
+def start(bot, update):
+    bot.send_message(chat_id = update.message.chat_id , text = "salam 🌹 , khosh oomadi {}  \n\n\n\n  /start--> salam\n /admin--> admin control \n /help--> help\n /info--> info".format(update.message.from_user.first_name))
+    print(update)
+    print("---------------")
+
+def helper(bot, update):
+    bot.send_message(chat_id = update.message.chat_id , text = "help help help help help help help ")
+
+def info(bot, update):
+    bot.send_message(chat_id = update.message.chat_id , text = "info info info info info info info  ")
+
+def admin_control(bot, update):
+    if (update.message.chat_id == 378089614 ):  # if (update.message.from_user.first_name == "@nimadorostkar" )
+        bot.send_message(chat_id = update.message.chat_id , text = "✅ admin find ✅  ")
+        bot.send_message(chat_id = update.message.chat_id , text = "✅ update ! ✅ ")
+    else:
+        bot.send_message(chat_id = update.message.chat_id , text = " ❌ error ❌ ")
+
+def message_filter(bot, update):
+    bot.send_message(chat_id = update.message.chat_id , text = "شما گفتید : {}".format(update.message.text))
+
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('help', helper))
+updater.dispatcher.add_handler(CommandHandler('info', info))
+updater.dispatcher.add_handler(CommandHandler('admin', admin_control))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message_filter))
+
+print("server is runing . . . ")
+
+updater.start_polling()
+updater.idle()
